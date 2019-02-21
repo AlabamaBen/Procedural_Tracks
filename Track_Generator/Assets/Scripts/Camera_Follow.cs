9ff47390bad2046ef47f6e26c1054514f2253833
AlabamaBen <benjamin.b.morelle@gmail.com>
2019-02-21 02:30:43 +0100

    First Commit

---
+++ Track_Generator/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+	public Transform target_position;
+    public Transform target_lookat;
+
+    public bool look_at = true; 
+
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+	void FixedUpdate ()
+	{
+		Vector3 desiredPosition = target_position.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+		transform.position = smoothedPosition;
+		if(look_at)
+		{
+			transform.LookAt(target_lookat);
+		}
+	}
+}
